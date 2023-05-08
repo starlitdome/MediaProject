f3fb364e631dc557fe5c6bf3b97e6eea632b6289
Taffy B <129110718+starlitdome@users.noreply.github.com>
2023-05-08 15:43:47 -0400

    Add files via upload

---
+++ p5project/sketch.js
@@ -0,0 +1,74 @@
+let waveAmt;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  waveAmt = map(mouseY, 0 , height, 0,-11, 1);
+  background(200);
+  // three-digit hexadecimal RGB notation
+background('#fae');
+describe('canvas with pink background');
+  // Draw a circle at location (30, 30) with a diameter of 20.
+  fill(111,78,55);
+circle(150,100,40); 
+  fill(111,78,55);
+circle(250,100,40)
+  fill(193,154,107)
+circle(150,100,23)
+  fill(193,154,107)
+circle(250,100,23)
+  fill(111,78,55)
+ellipse(170,230,160,30)
+  fill(111,78,55)
+ellipse(230,230 + waveAmt,160,30)  //left arm
+  fill(111,78,55)
+ellipse(200,250,130,120);
+  fill(111,78,55)
+ellipse(162,290,55,50)  
+  fill(111,78,55)
+circle(200,155,135)
+  fill(111,78,55)
+ellipse(240,290,55,50) 
+  fill(0,0,0)
+ellipse(172,152,30,20)
+  fill(0,0,0)
+ellipse(228,152,30,20) 
+  fill(255,255,255)
+ellipse(178,150,10,10)
+  fill(255,255,255)
+ellipse(222,150,10,10)
+  fill(193,154,107)
+ellipse(200,190,36,45)  
+  fill(193,154,107)
+ellipse(147,286,16,16)
+  fill(193,154,107)
+ellipse(162,280,16,16)
+  fill(193,154,107)
+ellipse(177,286,16,16)
+  fill(193,154,107)
+ellipse(225,286,16,16)
+  fill(193,154,107)
+ellipse(240,280,16,16)
+  fill(193,154,107)
+ellipse(255,286,16,16)  
+  fill(0,0,0)
+circle(200,180,14)
+  fill(0,0,0)
+rect(199,180,2,15)  
+  fill(0,0,0)
+rect(190,194,20,3) 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
